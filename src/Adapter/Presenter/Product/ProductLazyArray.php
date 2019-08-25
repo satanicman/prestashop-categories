1d028a43fea79311c348fd12a17f8c2b0f9dc04c
Павел Тополь <p.topol@reactor.com.ua>
2019-08-25 16:44:46 +0300

    product flags

--- src/Adapter/Presenter/Product/ProductLazyArray.php
+++ src/Adapter/Presenter/Product/ProductLazyArray.php
@@ -446,22 +446,10 @@ class ProductLazyArray extends AbstractLazyArray
         }
 
         if ($show_price && $this->product['reduction']) {
-            if ($this->product['discount_type'] === 'percentage') {
-                $flags['discount'] = array(
-                    'type' => 'discount',
-                    'label' => $this->product['discount_percentage'],
-                );
-            } elseif ($this->product['discount_type'] === 'amount') {
-                $flags['discount'] = array(
-                    'type' => 'discount',
-                    'label' => $this->product['discount_amount_to_display'],
-                );
-            } else {
-                $flags['discount'] = array(
-                    'type' => 'discount',
-                    'label' => $this->translator->trans('Reduced price', array(), 'Shop.Theme.Catalog'),
-                );
-            }
+            $flags['discount'] = array(
+                'type' => 'discount',
+                'label' => $this->translator->trans('Reduced price', array(), 'Shop.Theme.Catalog'),
+            );
         }
 
         if ($this->product['new']) {
